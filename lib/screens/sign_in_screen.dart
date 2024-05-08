089c4cec2ee73b7d5a8c662bade9c7360e8eae2c
ValenLioe <vvalen.lioee@mhs.mdp.ac.id>
2024-05-08 18:33:03 +0700

    .

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,19 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-<<<<<<< HEAD
 import 'package:fasum/screens/home_screen.dart';
 import 'package:fasum/screens/sign_up_screen.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
-=======
-import 'package:sharesep/screens/home_screen.dart';
-import 'package:sharesep/screens/sign_up_screen.dart';
-
-class SignInScreen extends StatefulWidget {
-  const SignInScreen({super.key});
-
->>>>>>> cdf65f0ab753676e32943389115445867f8bf1c8
   @override
   SignInScreenState createState() => SignInScreenState();
 }
@@ -22,7 +13,6 @@ class SignInScreenState extends State<SignInScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   String _errorMessage = '';
-<<<<<<< HEAD
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -148,65 +138,3 @@ class SignInScreenState extends State<SignInScreen> {
     return regex.hasMatch(email);
   }
 }
-=======
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Sign In'),
-        ),
-        body: Padding(
-        padding: const EdgeInsets.all(16.0),
-    child: SingleChildScrollView(
-    child: Column(
-    children: [
-    const SizedBox(height: 32.0),
-    TextField(
-    controller: _emailController,
-    decoration: const InputDecoration(
-    labelText: 'Email',
-    border: OutlineInputBorder(),
-    ),
-    ),
-    const SizedBox(height: 16.0),
-    TextField(
-    controller: _passwordController,
-    decoration: const InputDecoration(
-    labelText: 'Password',
-    border: OutlineInputBorder(),
-    ),
-    obscureText: true,
-    ),
-    const SizedBox(height: 16.0),
-    ElevatedButton(
-    onPressed: () async {
-    final email = _emailController.text.trim();
-    final password = _passwordController.text;
-// Validasi email
-    if (email.isEmpty || !isValidEmail(email)) {
-    ScaffoldMessenger.of(context).showSnackBar(
-    const SnackBar(content: Text('Please enter a valid
-    email')),
-    );
-    return;
-    }
-// Validasi password
-    if (password.isEmpty) {
-    ScaffoldMessenger.of(context).showSnackBar(
-    const SnackBar(content: Text('Please enter your
-    password')),
-    );
-    return;
-    }
-    try {
-// Lakukan sign in dengan email dan password
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-    email: email,
-    password: password,
-    );
-// Jika berhasil sign in, navigasi ke halaman beranda
-    Navigator.of(context).pushReplacement(
-    MaterialPageRoute(builder: (context) => const
-    HomeScreen()),
->>>>>>> cdf65f0ab753676e32943389115445867f8bf1c8
